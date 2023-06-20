43fe3f53d2340b01d18ab177cb2777b0f0f79891
jonathankerth <jonathanpkerth@gmail.com>
2023-06-19 19:28:59 -0700

    add movies button to user profile page

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,24 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
+  favourites: any[] = []; // Store the user's favorite movies here
+
   constructor(public fetchApiData: FetchApiDataService) {}
 
   ngOnInit(): void {
     this.getMovies();
+    this.getUserFavourites();
   }
 
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
-      console.log(this.movies);
-      return this.movies;
     });
+  }
+
+  getUserFavourites(): void {
+    const username = localStorage.getItem('user');
+    if (username) {
+      this.fetchApiData.getFavouriteMovies(username).subscribe(
+        (resp: any) => {
+          this.favourites = resp || []; // Assign the response or an empty array
+        },
+        (error: any) => {
+          console.error('Error retrieving user favorites:', error);
+        }
+      );
+    } else {
+      console.error('Username not found in local storage');
+    }
+  }
+
+  isMovieFavorite(movieId: string): boolean {
+    return this.favourites.some((movie) => movie._id === movieId);
+  }
+
+  toggleFavoriteMovie(movieId: string): void {
+    const username = localStorage.getItem('user');
+    if (this.isMovieFavorite(movieId)) {
+      this.fetchApiData
+        .deleteMovieFromFavourites(username!, movieId)
+        .subscribe(() => {
+          // Movie removed from favorites list
+          this.favourites = this.favourites.filter(
+            (movie) => movie._id !== movieId
+          );
+        });
+    } else {
+      this.fetchApiData
+        .addMovieToFavourites(username!, movieId)
+        .subscribe(() => {
+          // Movie added to favorites list
+          this.favourites.push(
+            this.movies.find((movie) => movie._id === movieId)
+          );
+        });
+    }
   }
 }
